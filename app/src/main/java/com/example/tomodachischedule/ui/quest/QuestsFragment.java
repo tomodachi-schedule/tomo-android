669f34d454637106a7e007dfcfc5c85ea6b93e38
aca-cia <acaciaenator@gmail.com>
2020-01-11 02:42:14 -0800

    tried making menu w/ 4 items. am i doing this right

--- app/src/main/java/com/example/tomodachischedule/ui/quest/QuestsFragment.java
+++ app/src/main/java/com/example/tomodachischedule/ui/quest/QuestsFragment.java
@@ -1,4 +1,4 @@
-package com.example.tomodachischedule.ui.notifications;
+package com.example.tomodachischedule.ui.quest;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.tomodachischedule.R;
 
-public class NotificationsFragment extends Fragment {
+public class QuestsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private QuestsViewModel questsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        questsViewModel =
+                ViewModelProviders.of(this).get(QuestsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_prefs, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
+        questsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
